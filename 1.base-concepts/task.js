a7cca01a6ab41d6f2a68dd96393837b9899d1a48
Liubov Chudakova <chudakova.lyuba.00@gmail.com>
2021-12-07 15:54:42 +0300

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
+'use strict'
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let D = Math.pow(a, 2) - 4 * a * c;
+  if (D < 0) {
+    arr = [] ;
+    return arr;
+  } else if (D === 0) {
+    arr[0] = -b/(2 * a)
+    return arr;
+  } else {
+    arr[0] = (-b + Math.sqrt(D) )/(2 * a) 
+    arr[1] = (-b - Math.sqrt(D) )/(2 * a)
+    console.log(arr.length)
+    return arr;
+  }
+   // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
