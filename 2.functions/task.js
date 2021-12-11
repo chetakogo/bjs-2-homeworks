bb2232822dff82946e32622487ef82f26a7406a9
Liubov Chudakova <chudakova.lyuba.00@gmail.com>
2021-12-12 00:46:49 +0300

    first task

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,47 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+min = Infinity;
+max = -Infinity;
+sum = 0;
 
-  // Ваш код
-
+for ( i = 0; i < arr.length; i++ ) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+  if (arr[i] < min) {
+    min = arr[i];
+  }
+  sum += arr[i];
+}
+avg = sum / arr.length
+avg = avg.toFixed(2);
+avg = Number(avg)
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for ( i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
 
-  // Ваш кода
-  // for ...
+function makeWork(arrOfArr, func) {
+  let max = 0;
+  let sum;
+  func(worker)
+  for (i = 0; i < arrOfArr.length; i++) {
+  sum = func(arrOfArr[i])
+ if (sum > max) {
+   max = sum
+ }
+  }
   
   return max;
 }
