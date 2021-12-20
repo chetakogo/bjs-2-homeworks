6341acfff5d3ffff2dfcc58ef2efb89d35c733fa
Liubov Chudakova <chudakova.lyuba.00@gmail.com>
2021-12-20 22:23:10 +0300

    homework

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks =[];
+    this.marks.push(mark);
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...args) {
+
+  if (this.marks === undefined) {
+    this.marks =[];
+    args.map(el => this.marks.push(el));
+
+  } else {
+    args.map(el => this.marks.push(el));
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.map(el => sum += el);
+  return sum / this.marks.length
+  
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason
+  delete this.marks
+  delete this.subject
+}
